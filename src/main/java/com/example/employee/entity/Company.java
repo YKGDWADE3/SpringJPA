efcd1897a8fbe428e05c3d0967e6d52630308ec5
YKGDWADE3 <superwadeg3@gmail.com>
2018-05-07 12:06:32 +0800

    finish test1

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,33 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Company {
+    @Id
+    private int id;
+
+    private String companyId;
+
+    private int employeesNumber;
+
+    public Company(){}
+    public Company(int id, String companyId, int employeesNumber) {
+        this.id = id;
+        this.companyId = companyId;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,54 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+    @Id
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    private int companyId;
+
+    private int salary;
+
+    public Employee(){}
+    public Employee(String name, int age, String gender, int salary, int id,int companyId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
